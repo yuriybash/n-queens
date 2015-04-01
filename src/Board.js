71b35d7d99565621de322542f0e0ede973494ae2
yuriybash <yuriybash@gmail.com>
2015-04-01 15:22:28 -0700

    finished tests

--- src/Board.js
+++ src/Board.js
@@ -217,12 +217,35 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+
+      var queenCount = 0;
+
+        for(var x = 0; x < this.rows().length; x++){
+
+            queenCount += this.rows()[x][minorDiagonalColumnIndexAtFirstRow - x] || 0;
+        }
+
+        return queenCount > 1 ? true : false;
+
+
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+
+      for(var x = 0; x < this.rows().length; x++){
+        for(var y = 0; y < this.rows()[x].length; y++){
+          if(this.hasMinorDiagonalConflictAt(y)){
+            return true;
+          }
+        }
+
+      }
+      return false;
+
+
+
+
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
